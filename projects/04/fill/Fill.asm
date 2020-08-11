4336554f99ea5da4d4633887f2dce2a23bfc869e
Prasenjit Nag (Joy) <pnag@zendesk.com>
2020-08-11 21:43:06 +1000

    4th chapter complete

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -12,3 +12,74 @@
 // the screen should remain fully clear as long as no key is pressed.
 
 // Put your code here.
+@i
+M=0
+@0
+@n
+M=A
+@color
+M=-1
+@key
+M=0
+
+(MAIN_LOOP)
+  @CHANGE_SCREEN
+  0;JMP
+  
+  @MAIN_LOOP
+  0;JMP
+
+(CHANGE_SCREEN)
+  @8191
+  D=A
+  @n
+  M=D
+  @i
+  M=0
+
+  // addr = SCREEN
+  @SCREEN
+  D=A
+  @addr
+  M=D
+  
+  // determine pixel color
+  @KBD
+  D=M
+  @BLACK
+  D;JGT
+  @WHITE
+  0;JMP
+  
+  (BLACK)
+    @color
+    M=-1
+    @CHANGE_PIXEL_LOOP
+    0;JMP
+  
+  (WHITE)
+    @color
+    M=0
+    @CHANGE_PIXEL_LOOP
+    0;JMP
+  
+  (CHANGE_PIXEL_LOOP)
+    @i
+    D=M
+    @n
+    D=D-M
+    @MAIN_LOOP
+    D;JGT
+  
+    @color
+    D=M
+    @addr
+    A=M
+    M=D
+  
+    @i
+    M=M+1
+    @addr
+    M=M+1
+    @CHANGE_PIXEL_LOOP
+    0;JMP
